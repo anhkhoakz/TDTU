08369c9f7f1ba556c9841040abb68bfd3309d85a
anhkhoakz <113995598+anhkhoakz@users.noreply.github.com>
2023-04-28 12:53:13 +0700

    first commit

---
+++ Semester_1/Programming-Methodology/10 labs of C code/lab 1/ex8.c
@@ -0,0 +1,15 @@
+# include <stdio.h>
+int main(){
+	printf("Ten: Nguyen Huynh Anh Khoa\nMSSV: 522H0046\n");
+	printf("Nhap vao 2 so:\n");
+	int a, b, max = 0;
+	scanf("%d%d", &a, &b);
+	if (a > b){
+		max = a;
+	}
+	else{
+		max = b;
+	}
+	printf("%d la gia tri lon nhat", max);
+	return 0;
+}
---
+++ Semester_2/IntroOS/Week 1/myprogram.c
@@ -0,0 +1,8 @@
+# include <stdio.h>
+
+int main() {
+	char x[100];
+	scanf("%s", x);
+	printf("Hello %s", x);
+	return 0;
+}
