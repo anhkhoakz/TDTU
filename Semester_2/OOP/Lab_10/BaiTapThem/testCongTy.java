8baaee15dee0925ea13a270864c7d801f18a3f47
anhkhoakz <113995598+anhkhoakz@users.noreply.github.com>
2023-05-11 15:26:29 +0700

    Remove Wrong Test Cases and Add Data.txt

--- Semester_2/OOP/Lab_10/BaiTapThem/testCongTy.java
+++ Semester_2/OOP/Lab_10/BaiTapThem/testCongTy.java
@@ -1,17 +1,19 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class testCongTy {
     public static void main(String[] args) {
-        CongTy giayThoiTrang_1 = new GiayThoiTrang("A201", "Mua Xuan", "Lua", 12, "Do", 2022, 5, "Mui nhon", 2.4);
-        CongTy giayThoiTrang_2 = new GiayThoiTrang("A202", "Mua He", "Nhung", 7, "Xanh", 2023, 2, "Mui vuong", 2.2);
-        CongTy giayThoiTrang_3 = new GiayThoiTrang("A203", "Mua Thu", "Cam", 5, "Cam", 2021, 8, "Mui dep", 1.5);
-
-        CongTy giayTheThao_1 = new GiayTheThao("S401", "Van Dong Manh", "Vai", 41, "Den", 2023, "Running", 2.5);
-        CongTy giayTheThao_2 = new GiayTheThao("S401", "Van Dong Manh", "Vai", 41, "Den", 2023, "Running", 2.5);
-        CongTy giayTheThao_3 = new GiayTheThao("S401", "Van Dong Manh", "Vai", 41, "Den", 2023, "Running", 2.5);
-
-        CongTy depSandal_1 = new DepSandal();
-        CongTy depSandal_2 = new DepSandal();
-        CongTy depSandal_3 = new DepSandal();
-        CongTy depSandal_4 = new DepSandal();
-
+        try {
+            File file = new File ("data.txt");
+            Scanner reader = new Scanner(file);
+            while (reader.hasNextLine()) {
+                String data = reader.nextLine();
+                System.out.println(data);
+            }
+            reader.close();
+        } catch (FileNotFoundException e) {
+            System.err.println(e);
+        }
     }
 }
